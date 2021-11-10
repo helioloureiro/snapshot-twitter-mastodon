62ca27a0585e8284e3fbcd2745ab7eed7233d08f
Helio Loureiro <helio.loureiro@ericsson.com>
2021-11-10 17:12:53 +0100

    Major code refactoring.
        - Removed the threads right now.

--- weather-twitter.py
+++ weather-twitter.py
@@ -42,6 +42,7 @@ HOMEDIR =  os.environ.get("HOME")
 CONFIGURATION = f"{HOMEDIR}/.twitterc"
 SAVEDIR = f"{HOMEDIR}/weather"
 FAILDIR = f"{SAVEDIR}/images"
+WEATHERJSONFILE = "/tmp/weather.json"
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 DISCARDFRAMES = 10
@@ -56,8 +57,9 @@ TIMEOUT =  10 * 60 # 10 minutes
 PID = os.getpid()
 LOCKFILE = f"{LOCKDIR}/{LOCKPREFIX}.{PID}"
 plock = threading.Lock() # control print
-failed_img = "%s.failed_img.jpg" % sys.argv[0]
-error_tries = FAILCOUNTER
+
+
+
 start_time = time.time()
 
 def debug(msg):
@@ -113,8 +115,7 @@ class CameraInterface:
 class Unix:
     def __init__(self): None
 
-    @staticmethod
-    def lockpid():
+    def lockpid(self):
         """
         Create a pid based lock file.
         Return true to "locked" and false in case of failure (already in use).
@@ -142,288 +143,241 @@ class Unix:
             fd.write(f"{PID}\n")
         return True
 
-    @staticmethod
-    def unlockpid():
+    def unlockpid(self):
         if os.path.exists(LOCKFILE):
             debug("Removing lock")
             os.unlink(LOCKFILE)
 
 
-def get_content():
-    """
-    Retrieve weather information
-    """
-    global wth_key, wth_loc
+class WeatherScreenshot(object):
+    def __init__(self):
+        self.filename = None
+        debug("\n ### WeatherScreenshot [%s] ### " % time.ctime())
+        self.ReadConfig()
+        self.SetTimeStamp()
+        self.SetTimeStampAndSaveFileName()
 
-    debug("Getting text content")
-    timestamp = time.strftime("Date: %Y-%m-%d %H:%M", time.localtime())
-    msg = []
-    msg.append("Stockholm")
-    msg.append(timestamp)
+    def ReadConfig():
+        """
+        Configuration from file ~/.twitterc
+        """
+        cfg = configparser.ConfigParser()
+        debug(f"Reading configuration: {CONFIGURATION}")
+        if not os.path.exists(CONFIGURATION):
+            raise Exception(f"Failed to find configuration file {CONFIGURATION}")
+        cfg.read(CONFIGURATION)
 
-    if sys.argv[-1] == "dry-run":
-        debug("Saving queries making an internal response")
-        msg.append(u"Temperature: %d°C" % randint(0,35) )
-        msg.append(u"Summary: super nice day ever!!!" )
+        self.credentials = {
+            "twitter_cons_key" : cfg.get("TWITTER", "CONS_KEY"),
+            "twitter_cons_sec" : cfg.get("TWITTER", "CONS_SEC"),
+            "twitter_acc_key"  : cfg.get("TWITTER", "ACC_KEY"),
+            "twitter_acc_sec"  : cfg.get("TWITTER", "ACC_SEC"),
+            "forecast_io_key"  : cfg.get("FORECAST.IO", "KEY"),
+            "forecast_io_loc"  : cfg.get("FORECAST.IO", "LOCATION")
+        }
+
+    def SetTimeStampAndSaveFileName(self):
+        year = time.strftime("%Y", time.localtime())
+        month = time.strftime("%m", time.localtime())
+        self.timestamp = time.strftime("%Y-%m-%d_%H%M%S", time.localtime())
+        self.savefile = f"{SAVEDIR}/{year}/{month}/{timestamp}.jpg")
+
+    def CreateDirectories(self, filename):
+        directories = os.path.dirname(filename)
+        if not os.path.exists(directories):
+            os.makedirs(directories)
+
+    def GetPhoto(self, f = None, quality = None):
+        """
+        Photo aquisition
+        """
+        self.CreateDirectories(self.savefile)
+        debug(f"Saving file {self.savefile}")
+        cam = LibCameraInterface()
+        cam.get_image(filename)
+
+    def GetForecastFromWeb(self):
+        """
+        Retrieve weather information from forcast.io.
+        """
+        forecast_io_key = self.credentials["forecast_io_key"]
+        forecast_io_location = self.credentials["forecast_io_loc"]
+
+        debug(" * requesting json about weather")
+        url = f"https://api.darksky.net/forecast/{forecast_io_key}/{forecast_io_location}"
+        req = requests.get(url)
+        jdata = json.loads(req.text)
+
+        return jdata
+
+    def isThereWeatherJson(self):
+        return os.path.exists(WEATHERJSONFILE)
+
+    def isNotTooOldData(self, filename):
+        """
+        If file is older than 3 hours, it is considered outdated.
+        """
+        fileStats = os.stat(filename)
+        modificationTime = fileStats.st_mtime
+        timeNow = time.time()
+        deltaInHours = (timeNow - modificationTime)/(60*60)
+        if deltaInHours >= 3:
+            return True
+        return False
+
+    def SaveForecastData(self, jData):
+        """
+        Remove old file in order to create new file stats and save the data.
+        """
+        if os.path.exists(WEATHERJSONFILE):
+            os.unlink(WEATHERJSONFILE)
+        with open(WEATHERJSONFILE, 'w') as output:
+            output.write(json.dumps(WEATHERJSONFILE, indent=4))
+
+    def LoadSavedForecastData(self):
+        with open(WEATHERJSONFILE) as inputFile:
+            jData = json.loads(inputFile.read())
+        return jData
+
+    def GetWeatherForecast(self):
+        """
+        Check if forecast json file exists and isn't older than 3 hours.
+        Otherwise fetch from forecast.io and save it.
+        """
+        getNewData = False
+        if isThereWeatherJson():
+            if isNotTooOldData(WEATHERJSONFILE):
+                getNewData = False
+            else:
+                getNewData = True
+        else:
+            getNewData = True
+
+        if getNewData:
+            jData = self.GetForecastFromWeb()
+            self.SaveForecastData(jData)
+        else:
+            jData = self.LoadSavedForecastData()
+
+        debug(" * converting from Farenheit to Celsius")
+        summary = jData["currently"]["summary"]
+        temp = jData["currently"]["temperature"]
+        temp = Far2Celsius(temp)
+
+        msg = []
+        msg.append("Stockholm")
+        msg.append(self.timestamp)
+        msg.append(u"Temperature: %s°C" % temp)
+        msg.append("Summary: %s" %summary)
+
+        debug(" * * Weather update finished")
         return msg
 
-    debug(" * requesting json about weather")
-    url = "https://api.darksky.net/forecast/%s/%s" % (wth_key, wth_loc)
-    req = requests.get(url)
-    jdata = json.loads(req.text)
-
-    # this was just to follow up - not needed
-    #print jdata.keys()
-    #print jdata["currently"]
-
-    debug(" * converting from Farenheit to Celsius")
-    summary = jdata["currently"]["summary"]
-    temp = jdata["currently"]["temperature"]
-    temp = Far2Celsius(temp)
-
-    msg.append(u"Temperature: %s°C" % temp)
-    msg.append("Summary: %s" %summary)
-
-    debug(" * * Weather update finished")
-    return msg
-
-def getfailedimg():
-    """
-    Get an alternative image in case of failure
-    """
-    if not os.path.exists(FAILDIR):
-        debug("Directory not found: %s" % FAILDIR)
-        return None
-    IMGS = []
-    for filename in os.listdir(FAILDIR):
-        if not re.search("jpg|gif|png", filename):
-            continue
-        IMGS.append("%s/%s" % (FAILDIR, filename) )
-    # get a randomic one
-    pos = randint(0, len(IMGS) - 1)
-    chosen_one = IMGS[pos]
-    copy(chosen_one, failed_img)
-    return failed_img
-
-def ReadConfig():
-    """
-    Configuration from file ~/.twitterc
-    """
-    global cons_key, cons_sec, acc_key, acc_sec, wth_key, wth_loc
-
-    cfg = configparser.ConfigParser()
-    debug(f"Reading configuration: {CONFIGURATION}")
-    if not os.path.exists(CONFIGURATION):
-        raise Exception(f"Failed to find configuration file {CONFIGURATION}")
-    cfg.read(CONFIGURATION)
-    cons_key = cfg.get("TWITTER", "CONS_KEY")
-    cons_sec = cfg.get("TWITTER", "CONS_SEC")
-    acc_key = cfg.get("TWITTER", "ACC_KEY")
-    acc_sec = cfg.get("TWITTER", "ACC_SEC")
-    wth_key = cfg.get("FORECAST.IO", "KEY")
-    wth_loc = cfg.get("FORECAST.IO", "LOCATION")
-
-def GetPhoto(f = None, quality = None):
-    """
-    Photo aquisition
-    """
-    global filename, FAILCOUNTER, error_tries
-
-    debug("GetPhoto: failcounter=%d" % FAILCOUNTER)
-    if FAILCOUNTER < 0:
-        print("Fail counter reached maximum attempts.  Failed.")
-        debug("Trying to return a failed img.")
-        filename = getfailedimg()
-        if not filename:
-            sys.exit(1)
-        debug("Using failed img: %s" % filename)
-        # it needs to be 0ed or it will fail
-        FAILCOUNTER = 0
-        return 0
-    filename = None
-    #if not os.path.exists(SAVEDIR):
-    #    os.makedirs(SAVEDIR)
-    year = time.strftime("%Y", time.localtime())
-    month = time.strftime("%m", time.localtime())
-    if not os.path.exists("%s/%s/%s" % (SAVEDIR, year, month)):
-        os.makedirs("%s/%s/%s" % (SAVEDIR, year, month) )
-    if not f:
-        timestamp = time.strftime("%Y-%m-%d_%H%M%S", time.localtime())
-        filename = "%s/%s/%s/%s.jpg" % (SAVEDIR, year, month, timestamp)
-    else:
-        filename = f
-    debug("Saving file %s" % filename)
-    cam = LibCameraInterface()
-    cam.get_image(filename)
-
-def TheWalkingDead(walker=None):
-    """
-    After a certain time, it is sure task won't be completed.
-    So kill it.
-    """
-    if not walker:
-        return
-    count = 0
-    if sys.version_info.major < 3:
-        raise Exception("Python version not supported.  Move to Python3.")
-
-    if sys.version_info.minor <= 8:
-        check = walker.isAlive
-    else:
-        check = walker.is_alive
-    while(check()):
-        time.sleep(random())
-        current_time = time.time()
-        delta = current_time - start_time
-        # print "Thread(%s) counter=%d delta=%0.2f" % (walker, count, delta)
-        if delta > TIMEOUT:
-            debug("Reached timeout.  Killing pid")
-            try:
-                os.kill(PID, 9)
-            except ProcessLookupError:
-                # process is already killed
-                pass
-            count += 1
-
-
-def WeatherScreenshot():
-    global filename
-
-    debug("\n ### WeatherScreenshot [%s] ### " % time.ctime())
-    debug("Threading image acquisition")
-    th = threading.Thread(target=GetPhoto)
-    th.start()
-    twd = threading.Thread(target=TheWalkingDead, args=(th,))
-    twd.walker = "Testing"
-    twd.start()
-    if not twd.is_alive():
-        sys.exit(1)
-
-    ReadConfig()
-
-    debug("Autenticating in Twitter")
-    # App python-tweeter
-    # https://dev.twitter.com/apps/815176
-    tw = twitter.Api(
-        consumer_key = cons_key,
-        consumer_secret = cons_sec,
-        access_token_key = acc_key,
-        access_token_secret = acc_sec
-        )
-    debug("Retrieving info...")
-    msg = get_content()
-    th.join()
-    twd.join()
-
-    if FAILCOUNTER < 0:
-        print("Failed to acquire image.  Quitting...")
-        sys.exit(1)
-    if not msg:
-        msg = "Just another shot at %s" % \
-            time.strftime("%H:%M", time.localtime())
-    if msg:
-        im = Image.open(filename)
-        # just get truetype fonts on package ttf-mscorefonts-installer
+    def SetImageFont(self):
+        """
+        Just get truetype fonts on package ttf-mscorefonts-installer.
+        """
         try:
-            f_top = ImageFont.truetype(font="Impact", size=60)
+            fontHead = ImageFont.truetype(font="Impact", size=60)
         except TypeError:
             # older versions hasn't font and require full path
             arialpath = "/usr/share/fonts/truetype/msttcorefonts/Impact.ttf"
-            f_top = ImageFont.truetype(arialpath, size=60)
+            fontHead = ImageFont.truetype(arialpath, size=60)
         try:
-            f_body = ImageFont.truetype(font="Arial", size=20)
+            fontBody = ImageFont.truetype(font="Arial", size=20)
         except TypeError:
             # older versions hasn't font and require full path
             arialpath = "/usr/share/fonts/truetype/msttcorefonts/Arial.ttf"
-            f_body = ImageFont.truetype(arialpath, size=20)
+            fontBody = ImageFont.truetype(arialpath, size=20)
+        return fontHead, fontBody
 
-        """
-        # SHADOW
-        step = 1
+    def UpdateImageWithText(self, messageArray, imageFile):
+        image = Image.open(imageFile)
 
-        for c in [ WHITE, BLACK ]:
-            txt = Image.new('L', IMGSIZE)
-            d = ImageDraw.Draw(txt)
-            d.text( (10 + step, 10 + step), msg[0], font=f_top, fill=255)
-            position = 80
-            for m in msg[1:]:
-                d.text( (10 + step, position + step), m, font=f_body, fill=255)
-                position += 20
-            w = txt.rotate(0, expand=1)
-            step = 0
-            im.paste(ImageOps.colorize(w, c, c), (0,0), w)
-            im.save(filename)
-        """
+        fontHead, fontBody = self.SetImageFont()
+
         step = 0
         debug("Writting in WHITE")
-        txt = Image.new('L', IMGSIZE)
+        text = Image.new('L', IMGSIZE)
         d = ImageDraw.Draw(txt)
 
-        ## Title ##
+        ## Head ##
         # border first
-        d.text((10 + step + 1, 10 + step), msg[0], font=f_top, fill=255)
-        d.text((10 + step - 1, 10 + step), msg[0], font=f_top, fill=255)
-        d.text((10 + step, 10 + step + 1), msg[0], font=f_top, fill=255)
-        d.text((10 + step, 10 + step - 1), msg[0], font=f_top, fill=255)
+        d.text((10 + step + 1, 10 + step), messageArray[0], font=fontHead, fill=255)
+        d.text((10 + step - 1, 10 + step), messageArray[0], font=fontHead, fill=255)
+        d.text((10 + step, 10 + step + 1), messageArray[0], font=fontHead, fill=255)
+        d.text((10 + step, 10 + step - 1), messageArray[0], font=fontHead, fill=255)
 
         ## Body ##
         position = 80
-        for m in msg[1:]:
+        for m in messageArray[1:]:
             # border first
-            d.text( (10 + step + 1, position + step), m, font=f_body, fill=255)
-            d.text( (10 + step - 1, position + step), m, font=f_body, fill=255)
-            d.text( (10 + step, position + step + 1), m, font=f_body, fill=255)
-            d.text( (10 + step, position + step - 1), m, font=f_body, fill=255)
+            d.text( (10 + step + 1, position + step), m, font=fontBody, fill=255)
+            d.text( (10 + step - 1, position + step), m, font=fontBody, fill=255)
+            d.text( (10 + step, position + step + 1), m, font=fontBody, fill=255)
+            d.text( (10 + step, position + step - 1), m, font=fontBody, fill=255)
             # content
-            d.text( (10 + step, position + step), m, font=f_body, fill=255)
+            d.text( (10 + step, position + step), m, font=fontBody, fill=255)
             position += 20
 
         # final touch
-        w = txt.rotate(0, expand=1)
-        im.paste(ImageOps.colorize(w, WHITE, WHITE), (0,0), w)
-        im.save(filename)
+        w = text.rotate(0, expand=1)
+        image.paste(ImageOps.colorize(w, WHITE, WHITE), (0,0), w)
+        image.save(imageFile)
 
         debug("Writting in BLACK")
-        txt = Image.new('L', IMGSIZE)
+        text = Image.new('L', IMGSIZE)
         d = ImageDraw.Draw(txt)
-        # content
-        d.text((10 + step, 10 + step), msg[0], font=f_top, fill=255)
+
+        # Head
+        d.text((10 + step, 10 + step), messageArray[0], font=fontHead, fill=255)
+
+        # Body
         position = 80
-        for m in msg[1:]:
+        for m in messageArray[1:]:
             # content
-            d.text( (10 + step, position + step), m, font=f_body, fill=255)
+            d.text( (10 + step, position + step), m, font=fontBody, fill=255)
             position += 20
 
         # final touch
         w = txt.rotate(0, expand=1)
         im.paste(ImageOps.colorize(w, BLACK, BLACK), (0,0), w)
-        im.save(filename)
+        im.save(imageFile)
+
+    def SendTwitter(self):
+        debug("Autenticating in Twitter")
+        # App python-tweeter
+        # https://dev.twitter.com/apps/815176
+        tw = twitter.Api(
+            consumer_key = self.credentials["twitter_cons_key"],
+            consumer_secret = self.credentials["twitter_cons_sec"],
+            access_token_key = self.credentials["twitter_acc_key"],
+            access_token_secret = self.credentials["twitter_acc_sec"]
+            )
+        debug("Retrieving info...")
+        imageText = GetWeatherForecast()
+
+        if imageText is None:
+            imageText = [ f"Just another shot at {self.timestamp}" ]
+        else:
+            self.UpdateImageWithText(imageText, self.savefile)
 
         # adding the credit to the right guys (awesome guys btw)
-        msg = u"%s \nvia http://forecast.io/#/f/59.4029,17.9436" % "\n".join(msg)
+        imageText.append("via http://forecast.io/#/f/59.4029,17.9436")
+        twitterText = "\n".join(imageText)
         try:
-            print(u"%s" % msg)
-        except UnicodeEncodeError:
-            # I just hate this...
-            pass
-        if sys.argv[-1] == "dry-run":
-            debug("Just dry-run mode.  Done!")
-            return
-        try:
-            tw.PostUpdate(status = msg,media = filename)
+            tw.PostUpdate(status = twitterText, media = self.savefile)
             debug("done!")
         except Exception as e:
             print("Failed for some reason:", e)
-            # it failed so... deal w/ it.
-            pass
-    else:
-        print("no message available")
 
 
 if __name__ == '__main__':
     try:
         if Unix.lockpid():
-            WeatherScreenshot()
+            shot = WeatherScreenshot()
+            shot.GetPhoto()
+            shot.SendTwitter()
             Unix.unlockpid()
     except KeyboardInterrupt:
         sys.exit(0)
