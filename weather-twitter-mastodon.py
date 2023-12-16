a403b606afc4eb7bbbb3536fec6b13c71991bd25
Helio Loureiro <helio.loureiro@ericsson.com>
2023-12-16 17:38:53 +0100

    From 0.8 to 0.5

--- weather-twitter-mastodon.py
+++ weather-twitter-mastodon.py
@@ -186,7 +186,7 @@ class LibCameraInterface:
             "/usr/bin/rpicam-still",
             "--width=" + str(width),
             "--height=" + str(height),
-            "--brightness=0.8",
+            "--brightness=0.5,
             "--exposure=normal",
             "-o",
              destination
@@ -201,7 +201,7 @@ class LibCameraInterface:
             "/usr/bin/rpicam-still",
             "--width=" + str(width),
             "--height=" + str(height),
-            "--brightness=-0.8",
+            "--brightness=-0.5",
             "--exposure=sport",
             "-o",
              destination
